f2cbf6e2260cbc406ce7a56f62356f3dff910bec
Ali Emad <aliemad43210@gmail.com>
2026-05-16 16:00:58 +0300

    Printing Matrices

--- Building_a_Parallel_System.c
+++ Building_a_Parallel_System.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <mpi.h>
 #include <omp.h>
+#include "cuda_worker.h"
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
@@ -22,61 +23,59 @@ long long factorial(int n)
 /* ================= STRING ================= */
 void reverse_string(char *str)
 {
-    int n = strlen(str);
+    int n=strlen(str);
 
     #pragma omp parallel for
     for(int i=0;i<n/2;i++)
     {
-        char temp = str[i];
-        str[i] = str[n-i-1];
-        str[n-i-1] = temp;
+        char t=str[i];
+        str[i]=str[n-i-1];
+        str[n-i-1]=t;
     }
 }
 
 int count_vowels(char *str)
 {
-    int count = 0;
-    int len = strlen(str);
+    int count=0;
+    int len=strlen(str);
 
     #pragma omp parallel for reduction(+:count)
     for(int i=0;i<len;i++)
     {
-        char c = tolower(str[i]);
+        char c=tolower(str[i]);
         if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
             count++;
     }
-
     return count;
 }
 
 /* ================= FILE ================= */
 void split_file(char *filename)
 {
-    FILE *f = fopen(filename,"r");
-    FILE *even = fopen("even.txt","w");
-    FILE *odd = fopen("odd.txt","w");
+    FILE *f=fopen(filename,"r");
+    if(!f){ printf("File not found!\n"); return; }
 
-    if(!f)
-    {
-        printf("File not found!\n");
-        return;
-    }
+    FILE *even=fopen("even.txt","w");
+    FILE *odd=fopen("odd.txt","w");
 
     char lines[200][256];
-    int count = 0;
+    int count=0;
 
-    while(count < 200 && fgets(lines[count],256,f))
+    while(count<200 && fgets(lines[count],256,f))
         count++;
 
     #pragma omp parallel for
     for(int i=0;i<count;i++)
     {
-        #pragma omp critical
+        if(i%2==0)
         {
-            if(i%2==0)
-                fputs(lines[i],even);
-            else
-                fputs(lines[i],odd);
+            #pragma omp critical(even_write)
+            fputs(lines[i],even);
+        }
+        else
+        {
+            #pragma omp critical(odd_write)
+            fputs(lines[i],odd);
         }
     }
 
@@ -86,48 +85,64 @@ void split_file(char *filename)
 }
 
 /* ================= MATRIX ================= */
-void matrix_add(double A[N][N], double B[N][N],
-                double C[N][N], int start, int rows)
+void matrix_add(double A[N][N],
+                double B[N][N],
+                double C[N][N],
+                int start,int rows)
 {
     #pragma omp parallel for collapse(2)
     for(int i=start;i<start+rows;i++)
         for(int j=0;j<N;j++)
-            C[i][j] = A[i][j] + B[i][j];
+            C[i][j]=A[i][j]+B[i][j];
+}
+
+void print_matrix(double M[N][N], const char *name)
+{
+    printf("\n===== %s (5x5 sample) =====\n", name);
+
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j = 0; j < 5; j++)
+        {
+            printf("%6.1f ", M[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 /* ================= MAIN ================= */
-int main(int argc, char** argv)
+int main(int argc,char** argv)
 {
-    int rank, size;
+    setbuf(stdout,NULL);
+
+    int rank,size;
 
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     MPI_Comm_size(MPI_COMM_WORLD,&size);
 
-    /* SAFE CHECK */
-    if(size < 10)
+    if(size!=11)
     {
-        if(rank == 0)
-            printf("ERROR: Run with at least 10 processes\n");
-
+        if(rank==0)
+            printf("Run using:\nmpirun --oversubscribe -np 11 ./program\n");
         MPI_Finalize();
         return 0;
     }
 
     /* ================= MASTER ================= */
-    if(rank == 0)
+    if(rank==0)
     {
-        int number = 6;
-        char str[] = "parallel";
-        char file[] = "input.txt";
+        int number=6;
+        char str[]="parallel";
+        char file[]="input.txt";
 
-        double A[N][N], B[N][N];
+        double A[N][N],B[N][N];
 
         for(int i=0;i<N;i++)
             for(int j=0;j<N;j++)
             {
-                A[i][j] = i + j;
-                B[i][j] = i * j;
+                A[i][j]=i+j;
+                B[i][j]=i*j;
             }
 
         MPI_Send(&number,1,MPI_INT,1,0,MPI_COMM_WORLD);
@@ -151,86 +166,102 @@ int main(int argc, char** argv)
         double C[N][N];
         MPI_Recv(C,N*N,MPI_DOUBLE,4,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 
+        /* CUDA PROCESS */
+        double GPU_A[N][N],GPU_C[N][N];
+
+        for(int i=0;i<N;i++)
+            for(int j=0;j<N;j++)
+                GPU_A[i][j]=i+j;
+
+        MPI_Send(GPU_A,N*N,MPI_DOUBLE,10,0,MPI_COMM_WORLD);
+        MPI_Recv(GPU_C,N*N,MPI_DOUBLE,10,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+
         printf("\n===== FINAL RESULTS =====\n");
         printf("Factorial = %lld\n",fact);
         printf("Reversed String = %s\n",resultStr);
         printf("Vowels = %d\n",vowels);
         printf("%s\n",msg);
-        printf("Matrix Addition Done\n");
+        print_matrix(A, "Matrix A");
+        print_matrix(B, "Matrix B");
+        print_matrix(C, "Matrix Addition Result");
+        print_matrix(GPU_C, "CUDA Matrix Result");
     }
 
-    else if(rank == 1)
+    /* INTEGER */
+    else if(rank==1)
     {
         int num;
         MPI_Recv(&num,1,MPI_INT,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-
-        long long result = factorial(num);
-
+        long long result=factorial(num);
         MPI_Send(&result,1,MPI_LONG_LONG,0,0,MPI_COMM_WORLD);
     }
 
-    else if(rank == 2)
+    /* STRING */
+    else if(rank==2)
     {
         char str[100];
-
         MPI_Recv(str,100,MPI_CHAR,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 
         reverse_string(str);
-        int vowels = count_vowels(str);
+        int vowels=count_vowels(str);
 
         MPI_Send(str,100,MPI_CHAR,0,0,MPI_COMM_WORLD);
         MPI_Send(&vowels,1,MPI_INT,0,0,MPI_COMM_WORLD);
     }
 
-    else if(rank == 3)
+    /* FILE */
+    else if(rank==3)
     {
         char filename[100];
-
         MPI_Recv(filename,100,MPI_CHAR,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 
         split_file(filename);
 
-        char msg[] = "File split finished";
-
+        char msg[]="File split finished";
         MPI_Send(msg,strlen(msg)+1,MPI_CHAR,0,0,MPI_COMM_WORLD);
     }
 
-    else if(rank == 4)
+    /* MATRIX MANAGER */
+    else if(rank==4)
     {
-        double A[N][N], B[N][N], C[N][N];
+        double A[N][N],B[N][N],C[N][N];
 
         MPI_Recv(A,N*N,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
         MPI_Recv(B,N*N,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 
-        int workers = size - 5;
-        if(workers <= 0) workers = 1;
+        int workers=5;
+        int rows=N/workers;
 
-        int rows = N / workers;
-
-        for(int p=5;p<size;p++)
+        for(int p=5;p<=9;p++)
         {
+            int start=(p-5)*rows;
+
             MPI_Send(A,N*N,MPI_DOUBLE,p,0,MPI_COMM_WORLD);
             MPI_Send(B,N*N,MPI_DOUBLE,p,0,MPI_COMM_WORLD);
             MPI_Send(&rows,1,MPI_INT,p,0,MPI_COMM_WORLD);
-
-            int start = (p-5)*rows;
             MPI_Send(&start,1,MPI_INT,p,0,MPI_COMM_WORLD);
         }
 
-        for(int p=5;p<size;p++)
+        for(int p=5;p<=9;p++)
         {
-            int start = (p-5)*rows;
-            MPI_Recv(&C[start][0],rows*N,MPI_DOUBLE,
-                     p,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+            int start=(p-5)*rows;
+
+            MPI_Recv(&C[start][0],
+                     rows*N,
+                     MPI_DOUBLE,
+                     p,0,
+                     MPI_COMM_WORLD,
+                     MPI_STATUS_IGNORE);
         }
 
         MPI_Send(C,N*N,MPI_DOUBLE,0,0,MPI_COMM_WORLD);
     }
 
-    else if(rank >= 5)
+    /* MATRIX WORKERS */
+    else if(rank>=5 && rank<=9)
     {
-        double A[N][N], B[N][N], C[N][N];
-        int rows, start;
+        double A[N][N],B[N][N],C[N][N];
+        int rows,start;
 
         MPI_Recv(A,N*N,MPI_DOUBLE,4,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
         MPI_Recv(B,N*N,MPI_DOUBLE,4,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
@@ -240,6 +271,20 @@ int main(int argc, char** argv)
         matrix_add(A,B,C,start,rows);
 
         MPI_Send(&C[start][0],rows*N,MPI_DOUBLE,4,0,MPI_COMM_WORLD);
+    }
+
+    /* CUDA WORKER */
+    else if(rank==10)
+    {
+        printf("CUDA worker started\n");
+
+        double A[N][N],C[N][N];
+
+        MPI_Recv(A,N*N,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+
+        cuda_matrix_double(&A[0][0],&C[0][0],N);
+
+        MPI_Send(C,N*N,MPI_DOUBLE,0,0,MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
